1a088de46a3e8921c3555e9be1722b13fa9f2634
Dae Choi <daechoi@outlook.com>
2024-06-06 09:22:29 -0400

    initial config

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    # NixOS official package source, using the nixos-24.05 branch here
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, nixvim, ...}: {
+    nixosConfigurations = {
+      dex = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # Import the previous configuration.nix we used,
+          # so the old configuration file still takes effect
+          ./configuration.nix
+	  home-manager.nixosModules.home-manager
+	  {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.extraSpecialArgs = {inherit inputs;};
+	    home-manager.users.dchoi = import ./home-manager;
+	  }
+      	];
+      };
+    };
+  };
+}
---
+++ home-manager/programs/default.nix
@@ -0,0 +1,199 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}: let
+  neovimconfig = import ../nixvim/config;
+  nvim = inputs.nixvim.legacyPackages.x86_64-linux.makeNixvimWithModule {
+    inherit pkgs;
+    module = neovimconfig;
+  };
+in {
+  home.packages = with pkgs; [
+    # here is some command line tools I use frequently
+    # feel free to add your own or remove some of them
+    nvim
+
+    # dev
+    go
+    rustup
+
+    htop
+    neofetch
+    nnn # terminal file manager
+
+    # archives
+    zip
+    xz
+    unzip
+    p7zip
+
+    # utils
+    ripgrep # recursively searches directories for a regex pattern
+    jq # A lightweight and flexible command-line JSON processor
+    yq-go # yaml processor https://github.com/mikefarah/yq
+    eza # A modern replacement for ‘ls’
+    fzf # A command-line fuzzy finder
+
+    # networking tools
+    mtr # A network diagnostic tool
+    iperf3
+    dnsutils # `dig` + `nslookup`
+    ldns # replacement of `dig`, it provide the command `drill`
+    aria2 # A lightweight multi-protocol & multi-source command-line download utility
+    socat # replacement of openbsd-netcat
+    nmap # A utility for network discovery and security auditing
+    ipcalc # it is a calculator for the IPv4/v6 addresses
+
+    # misc
+    cowsay
+    file
+    which
+    tree
+    gnused
+    gnutar
+    gawk
+    zstd
+    gnupg
+
+    # nix related
+    #
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
+
+    # productivity
+    hugo # static site generator
+    glow # markdown previewer in terminal
+
+    btop # replacement of htop/nmon
+    iotop # io monitoring
+    iftop # network monitoring
+
+    # system call monitoring
+    strace # system call monitoring
+    ltrace # library call monitoring
+    lsof # list open files
+
+    # system tools
+    sysstat
+    lm_sensors # for `sensors` command
+    ethtool
+    pciutils # lspci
+    usbutils # lsusb
+
+    chromium
+    foot
+  ];
+
+  wayland.windowManager.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+
+    config = {
+      terminal = "alacritty";
+      menu = "wofi --show run";
+      bars = [
+        {
+          fonts.size = 10.0;
+          position = "top";
+        }
+      ];
+      output = {
+        eDP-1 = {
+          scale = "1";
+        };
+      };
+    };
+  };
+
+  # basic configuration of git, please change to your own
+  programs = {
+    git = {
+      enable = true;
+      userName = "Dae Choi";
+      userEmail = "daechoi@outlook.com";
+      package = pkgs.gitAndTools.gitFull;
+      extraConfig = {
+        core.editor = "vim";
+        credential.helper = "cache";
+      };
+    };
+
+    foot = {
+      enable = true;
+      setttings = {
+        main = {
+          term = "xterm-256color-italic";
+        };
+      };
+    };
+
+    chromium.enable = true;
+
+    tmux = {
+      enable = true;
+      keyMode = "vi";
+      shortcut = "a";
+      terminal = "xterm-256color";
+      clock24 = true;
+      shell = "/etc/profiles/per-user/dchoi/bin/zsh";
+    };
+
+    alacritty = {
+      enable = true;
+      # custom settings
+      settings = {
+        env.TERM = "xterm-256color";
+        font = {
+          size = 12;
+        };
+        scrolling.multiplier = 5;
+        selection.save_to_clipboard = true;
+      };
+    };
+
+    # starship - an customizable prompt for any shell
+    starship = {
+      enable = true;
+      #custom settings
+      settings = {
+        add_newline = false;
+        aws.disabled = true;
+        gcloud.disabled = true;
+        line_break.disabled = true;
+      };
+    };
+
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+
+      shellAliases = {
+        ll = "ls -l";
+        update = "sudo nixos-rebuild switch";
+        vim = "nvim";
+      };
+      history = {
+        size = 10000;
+        path = "${config.xdg.dataHome}/zsh/history";
+      };
+
+      envExtra = "export TERM=xterm-256color";
+      initExtra = "source ~/.p10k.zsh";
+      zplug = {
+        enable = true;
+        plugins = [
+          {name = "zsh-users/zsh-autosuggestions";} # Simple plugin installation
+          {
+            name = "romkatv/powerlevel10k";
+          } # Installations with additional options. For the list of options, please refer to Zplug README.
+        ];
+      };
+    };
+    home-manager.enable = true;
+  };
+}
