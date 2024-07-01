610fb70a4d80195d7589eab0f72223dd96065bc6
Dae Choi <daechoi@outlook.com>
2024-07-01 12:39:42 -0400

    updates nixvim version

--- nixos-config/nixos/flake.nix
+++ nixos-config/nixos/flake.nix
@@ -13,7 +13,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
-      url = "github:nix-community/nixvim/release-24.05";
+      url = "github:nix-community/nixvim/nixos-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
