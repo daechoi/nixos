ce29944850ada77850c21aca055a8a77c086590f
Dae Choi <daechoi@outlook.com>
2024-09-26 21:48:11 +0000

    adds home manager

--- nix-wsl/flake.nix
+++ nix-wsl/flake.nix
@@ -1,10 +1,22 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixos-wsl = {
+	url = "github:nix-community/NixOS-WSL";
+	inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixvim = {
+	url = "github:nix-community/nixvim/nixos-24.05";
+	inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+	url = "github:nix-community/home-manager/release-24.05";
+	inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
-  outputs = { self, nixpkgs, nixos-wsl, ... }: {
+  outputs = inputs @ { nixpkgs, nixos-wsl, home-manager, nixvim, ... }: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -13,7 +25,17 @@
           {
             system.stateVersion = "24.05";
             wsl.enable = true;
+	    wsl.defaultUser = "dchoi";
           }
+	  ./configuration.nix
+	  home-manager.nixosModules.home-manager 
+	  {
+		home-manager = {
+			useGlobalPkgs = true;
+			useUserPackages = true;
+			users.dchoi = import ./home.nix;
+		};
+	  }
         ];
       };
     };
---
+++ nix-wsl/home-manager/nixvim/config/plug/lsp/lsp.nix
@@ -0,0 +1,127 @@
+{
+  plugins = {
+    lsp-format = {enable = true;};
+    lsp = {
+      enable = true;
+      servers = {
+        bashls = {enable = true;};
+        eslint = {enable = true;};
+        html = {enable = true;};
+        lua-ls = {enable = true;};
+        nil-ls = {enable = true;};
+        marksman = {enable = true;};
+        pyright = {enable = true;};
+        gopls = {enable = true;};
+        terraformls = {enable = true;};
+        tsserver = {
+          enable = true;
+          filetypes = ["javascript" "javascriptreact" "javascript.jsx" "typescript" "typescriptreact" "typescript.tsx"];
+          extraOptions = {
+            settings = {
+              javascript = {
+                inlayHints = {
+                  includeInlayEnumMemberValueHints = true;
+                  includeInlayFunctionLikeReturnTypeHints = true;
+                  includeInlayFunctionParameterTypeHints = true;
+                  includeInlayParameterNameHints = true;
+                  includeInlayParameterNameHintsWhenArgumentMatchesName = true;
+                  includeInlayPropertyDeclarationTypeHints = true;
+                  includeInlayVariableTypeHints = true;
+                };
+              };
+              typescript = {
+                inlayHints = {
+                  includeInlayEnumMemberValueHints = true;
+                  includeInlayFunctionLikeReturnTypeHints = true;
+                  includeInlayFunctionParameterTypeHints = true;
+                  includeInlayParameterNameHints = true;
+                  includeInlayParameterNameHintsWhenArgumentMatchesName = true;
+                  includeInlayPropertyDeclarationTypeHints = true;
+                  includeInlayVariableTypeHints = true;
+                };
+              };
+            };
+          };
+        };
+        yamlls = {
+          enable = true;
+        };
+      };
+      keymaps = {
+        silent = true;
+        lspBuf = {
+          gd = {
+            action = "definition";
+            desc = "Goto Definition";
+          };
+          gr = {
+            action = "references";
+            desc = "Goto References";
+          };
+          gD = {
+            action = "declaration";
+            desc = "Goto Declaration";
+          };
+          gI = {
+            action = "implementation";
+            desc = "Goto Implementation";
+          };
+          gT = {
+            action = "type_definition";
+            desc = "Type Definition";
+          };
+          K = {
+            action = "hover";
+            desc = "Hover";
+          };
+          "<leader>cw" = {
+            action = "workspace_symbol";
+            desc = "Workspace Symbol";
+          };
+          "<leader>cr" = {
+            action = "rename";
+            desc = "Rename";
+          };
+        };
+        diagnostic = {
+          "<leader>cd" = {
+            action = "open_float";
+            desc = "Line Diagnostics";
+          };
+          "[d" = {
+            action = "goto_next";
+            desc = "Next Diagnostic";
+          };
+          "]d" = {
+            action = "goto_prev";
+            desc = "Previous Diagnostic";
+          };
+        };
+      };
+    };
+    rust-tools.enable = true;
+  };
+  extraConfigLua = ''
+    local _border = "rounded"
+
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+      vim.lsp.handlers.hover, {
+        border = _border
+      }
+    )
+
+    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+      vim.lsp.handlers.signature_help, {
+        border = _border
+      }
+    )
+
+    vim.diagnostic.config{
+      float={border=_border}
+    };
+
+    require('lspconfig.ui.windows').default_options = {
+      border = _border
+    }
+  '';
+}
