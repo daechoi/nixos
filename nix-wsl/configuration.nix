4c8046d9bf01fa5a2b946ff8d77ee8f445a1baf7
Dae Choi <daechoi@outlook.com>
2024-09-27 00:11:20 -0400

    Updates timezone

--- nix-wsl/configuration.nix
+++ nix-wsl/configuration.nix
@@ -1,32 +1,38 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
-
-{ config, lib, pkgs, ... }:
-
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   environment.systemPackages = with pkgs; [
+    git
+    vim
+    wget
+    curl
 
-	git
-	vim
-	wget
-	curl
-	
-	tmux
-	zsh
+    tmux
+    zsh
   ];
 
   users.users.dchoi = {
-	extraGroups = ["wheel" "docker"];
-	shell = pkgs.zsh;
+    extraGroups = ["wheel" "docker"];
+    shell = pkgs.zsh;
+  };
+
+  time = {
+    timeZone = "America/New_York";
+    hardwareClockInLocalTime = false;
   };
 
   programs = {
-	zsh.enable=true;
+    zsh.enable = true;
   };
+
   nix.settings.experimental-features = ["nix-command" "flakes"];
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
