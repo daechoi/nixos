668e68ec5363f172c5942ae7694c31262b576fd1
Dae Choi <daechoi@outlook.com>
2024-11-21 21:30:43 -0500

    removes ssh

--- nix-mair/flake.nix
+++ nix-mair/flake.nix
@@ -27,21 +27,11 @@
     pkgs = import nixpkgs {
       inherit system;
     };
+
     lib = pkgs.lib;
-    configuration = {...}: {
+    configuration = {pkgs, ...}: {
       services = {
         nix-daemon.enable = true;
-        openssh = {
-          enable = true;
-          ports = [22];
-          settings = {
-            PasswordAuthentication = false;
-            AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
-            UseDns = true;
-            X11Forwarding = false;
-            PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
-          };
-        };
         yabai = {
           enable = true;
           package = pkgs.yabai;
