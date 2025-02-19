5fe3938d265c1d95494256c231bc4e25ee71bbff
Dae Choi <daechoi@outlook.com>
2025-02-18 22:14:47 -0500

    updates hlchunk

--- sabi/home-manager/nixvim/config/plug/lsp/hlchunk.nix
+++ sabi/home-manager/nixvim/config/plug/lsp/hlchunk.nix
@@ -5,46 +5,42 @@
       src = pkgs.fetchFromGitHub {
         owner = "shellRaining";
         repo = "hlchunk.nvim";
-        rev = "882d1bc86d459fa8884398223c841fd09ea61b6b";
-        hash = "sha256-fvFvV7KAOo7xtOCjhGS5bDUzwd10DndAKs3++dunED8=";
       };
-      meta.homepage = "https://github.com/shellRaining/hlchunk.nvim";
+
+      # Add runtime paths explicitly
+      buildPhase = ''
+        mkdir -p $out/lua
+        cp -r lua/* $out/lua/
+      '';
     })
   ];
 
-  # Add the plugin configuration
-  plugins.hlchunk = {
-    enable = true;
-    # Ensure lua/ directory is in runtimepath
-    extraConfig = ''
-      lua << EOF
-      require('hlchunk').setup({
-        chunk = {
-          enable = true,
-          support_filetypes = {
-            '.*', -- supports all filetypes
-          },
-          chars = {
-            horizontal_line = "─",
-            vertical_line = "│",
-            left_top = "╭",
-            left_bottom = "╰",
-            right_arrow = ">",
-          },
-          style = "#00ffff",
+  extraConfigLua = ''
+    require('hlchunk').setup({
+      chunk = {
+        enable = true,
+        support_filetypes = {
+          '.*',
         },
-        indent = {
-          enable = true,
-          use_treesitter = false,
+        chars = {
+          horizontal_line = "─",
+          vertical_line = "│",
+          left_top = "╭",
+          left_bottom = "╰",
+          right_arrow = ">",
         },
-        line_num = {
-          enable = true,
-        },
-        blank = {
-          enable = true,
-        },
-      })
-      EOF
-    '';
-  };
+        style = "#00ffff",
+      },
+      indent = {
+        enable = true,
+        use_treesitter = false,
+      },
+      line_num = {
+        enable = true,
+      },
+      blank = {
+        enable = true,
+      },
+    })
+  '';
 }
